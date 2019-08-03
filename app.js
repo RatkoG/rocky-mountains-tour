d15b7137fa81895fb346e0856f547886d7d9380c
ratko <ratko.mkd@outlook.com>
2019-08-03 10:35:18 -0600

    One button is working

---
+++ app.js
@@ -0,0 +1,4 @@
+document.querySelector('.navigation__item').addEventListener('click', function () {
+  console.log('It works, Add task');
+  document.getElementById("nav-toggle").checked = false;
+});
