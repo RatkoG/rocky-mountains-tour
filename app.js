dcc6d89423465ca7c363a3edd72b69c3c792be11
ratko <ratko.mkd@outlook.com>
2019-08-03 12:08:26 -0600

    Button is closing

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-document.querySelector('.navigation__item').addEventListener('click', function () {
-  console.log('It works, Add task');
-  document.getElementById("nav-toggle").checked = false;
-});
+const navMenu = document.querySelector('.navigation__list');
+
+function closeBtn(e) {
+  if (e.target.parentElement.classList.contains('navigation__item')) {
+    {
+      document.getElementById("nav-toggle").checked = false;
+    }
+  }
+  // console.log(e.target);
+  // console.log(e.target);// Ova e samo da vidime so naznacuvame
+}
+navMenu.addEventListener('click', closeBtn);
